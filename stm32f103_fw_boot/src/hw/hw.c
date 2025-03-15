ba9f8e0ede001f57606d1d8f8d4269fc8716e420
cho00 <cho00@DESKTOP-08EMFGO>
2025-03-16 03:46:57 +0900

    reset & dfu update

---
+++ stm32f103_fw_boot/src/hw/hw.c
@@ -0,0 +1,23 @@
+/*
+ * hw.c
+ *
+ *  Created on: Aug 22, 2024
+ *      Author: cho00
+ */
+
+
+#include "hw.h"
+
+
+
+// 모듈을 만든후 쓰기 위해 추가
+void hwInit(void)
+{
+  bspInit();
+  rtcInit();
+
+  resetInit();
+  ledInit();
+  usbInit();
+  flashInit();
+}
